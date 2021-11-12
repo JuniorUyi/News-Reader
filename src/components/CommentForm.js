2ca1b5cc0bbdcc6a27ccda8e7c2ebaa9f023f1e8
JuniorUyi <juniorjerry91@gmail.com>
2021-11-12 11:51:34 +0000

    Implemented my base for the article reader setup

---
+++ src/components/CommentForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+export default function CommentForm({ articleId }) {
+    const dispatch = useDispatch();
+    const [comment, setComment] = useState('');
+
+    // Declare isCreatePending here.
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // dispatch your asynchronous action here!
+        setComment('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label for='comment' className='label'>
+                Add Comment:
+            </label>
+            <div id='input-container'>
+                <input 
+                    id='comment'
+                    value={comment}
+                    onChange={(e) => setComment(e.currentTarget.value)}
+                    type='text'
+                />
+                <button className='comment-button'>Submit</button>
+            </div>
+        </form>
+    );
+}
